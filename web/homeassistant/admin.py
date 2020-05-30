2b553db8fef00f230589f17aff148fb4cbf93be1
Niels Berkers <niels.berkers@tradeface.nl>
2020-05-30 19:00:11 +0200

    tuya and ha

--- web/homeassistant/admin.py
+++ web/homeassistant/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Component, Variable
 
+
 class ComponentAdmin(admin.ModelAdmin):
     model = Component
-    list_display = ('name', 'technical_name')
-    #m:m Variables
+    list_display = ("name", "technical_name")
+    # m:m Variables
+
 
 admin.site.register(Component)
 admin.site.register(Variable)
