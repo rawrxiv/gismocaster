80c9426931cc3b4891a94ae06083d35d56218fa6
Niels Berkers <niels.berkers@tradeface.nl>
2020-06-12 10:37:04 +0200

    quick home page

--- web/mqtt/views.py
+++ web/mqtt/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "index.html")
